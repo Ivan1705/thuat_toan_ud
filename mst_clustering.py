ebfabdeff37e52a51b8ebd7dcd87c11b56723b4b
Ivan1705 <dophiquanghuy1705@gmail.com>
2024-06-30 22:13:18 +0700

    update final

--- mst_clustering.py
+++ mst_clustering.py
@@ -11,90 +11,40 @@ from scipy.sparse.csgraph._validation import validate_graph
 from sklearn.utils import check_array
 
 from sklearn.base import BaseEstimator, ClusterMixin
-from sklearn.neighbors import kneighbors_graph
 from sklearn.metrics import pairwise_distances
 
 
 class MSTClustering(BaseEstimator, ClusterMixin):
-    """Minimum Spanning Tree Clustering
-
-    Parameters
-    ----------
-    cutoff : float, int, optional
-        either the number of edges to cut (if cutoff >= 1) or the fraction of
-        edges to cut (if 0 < cutoff < 1). See also the ``cutoff_scale``
-        parameter.
-    cutoff_scale : float, optional
-        minimum size of edges. All edges larger than cutoff_scale will be
-        removed (see also ``cutoff`` parameter).
-    min_cluster_size : int (default: 1)
-        minimum number of points per cluster. Points belonging to smaller
-        clusters will be assigned to the background.
-    approximate : bool, optional (default: True)
-        If True, then compute the approximate minimum spanning tree using
-        n_neighbors nearest neighbors. If False, then compute the full
-        O[N^2] edges (see Notes, below).
-    n_neighbors : int, optional (default: 20)
-        maximum number of neighbors of each point used for approximate
-        Euclidean minimum spanning tree (MST) algorithm.  Referenced only
-        if ``approximate`` is False. See Notes below.
-    metric : string (default "euclidean")
-        Distance metric to use in computing distances. If "precomputed", then
-        input is a [n_samples, n_samples] matrix of pairwise distances (either
-        sparse, or dense with NaN/inf indicating missing edges)
-    metric_params : dict or None (optional)
-        dictionary of parameters passed to the metric. See documentation of
-        sklearn.neighbors.NearestNeighbors for details.
-
-    Attributes
-    ----------
-    full_tree_ : sparse array, shape (n_samples, n_samples)
-        Full minimum spanning tree over the fit data
-    T_trunc_ : sparse array, shape (n_samples, n_samples)
-        Non-connected graph over the final clusters
-    labels_: array, length n_samples
-        Labels of each point
-
-    Notes
-    -----
-    This routine uses an approximate Euclidean minimum spanning tree (MST)
-    to perform hierarchical clustering.  A true Euclidean minimum spanning
-    tree naively costs O[N^3].  Graph traversal algorithms only help so much,
-    because all N^2 edges must be used as candidates.  In this approximate
-    algorithm, we use k << N edges from each point, so that the cost is only
-    O[Nk log(Nk)]. For k = N, the approximation is exact; in practice for
-    well-behaved data sets, the result is exact for k << N.
-    """
     def __init__(self, cutoff=None, cutoff_scale=None, min_cluster_size=1,
                  approximate=True, n_neighbors=20,
                  metric='euclidean', metric_params=None):
         self.cutoff = cutoff
         self.cutoff_scale = cutoff_scale
         self.min_cluster_size = min_cluster_size
-        self.approximate = approximate
-        self.n_neighbors = n_neighbors
         self.metric = metric
         self.metric_params = metric_params
+        self.approximate = approximate
+        self.n_neighbors = n_neighbors
 
     def fit(self, X, y=None):
         """Fit the clustering model
 
         Parameters
         ----------
-        X : array_like
-            the data to be clustered: shape = [n_samples, n_features]
+        X : shape = [n_samples, n_features]
         """
         if self.cutoff is None and self.cutoff_scale is None:
             raise ValueError("Must specify either cutoff or cutoff_frac")
 
         # Compute the distance-based graph G from the points in X
         if self.metric == 'precomputed':
-            # Input is already a graph. Copy if sparse
-            # so we can overwrite for efficiency below.
+            # Input is already a graph
             self.X_fit_ = None
+            # check xem đồ thị X có hợp lệ không
             G = validate_graph(X, directed=True,
                                csr_output=True, dense_output=False,
                                copy_if_sparse=True, null_value_in=np.inf)
+        # Kiểm tra X có phải là array hợp lệ không (ma trận sparse của scipy hoặc mảng numpy)
         elif not self.approximate:
             X = check_array(X)
             self.X_fit_ = X
@@ -113,64 +63,55 @@ class MSTClustering(BaseEstimator, ClusterMixin):
                                  metric=self.metric,
                                  metric_params=self.metric_params)
 
-        # HACK to keep explicit zeros (minimum spanning tree removes them)
-        zero_fillin = G.data[G.data > 0].min() * 1E-8
-        G.data[G.data == 0] = zero_fillin
+        """Khi đồ thị có trọng số âm, các trọng số = 0 sẽ bị hiểu lầm trong tính toán vì thực chất nó vẫn có trọng số lớn hơn các cạnh âm khác"""
+        zero_fillin = G.data[G.data > 0].min() * 1E-8 # tùn gúa trị nhỏ nhất > 0 sau đó nhân với 1e8
+        G.data[G.data == 0] = zero_fillin # thay các giá trị 0 bằng giá trị rất nhỏ
 
-        # Compute the minimum spanning tree of this graph
+        # Thuật toán Kruskal xây dựng MST của scipy
         self.full_tree_ = minimum_spanning_tree(G, overwrite=True)
 
-        # undo the hack to bring back explicit zeros
+        # trả lại các giá tri 0
         self.full_tree_[self.full_tree_ == zero_fillin] = 0
 
-        # Partition the data by the cutoff
-        N = G.shape[0] - 1
-        if self.cutoff is None:
+        N = G.shape[0] - 1 # số cạnh trong MST
+        if self.cutoff is None: # giữ nguyên
             i_cut = N
         elif 0 <= self.cutoff < 1:
             i_cut = int((1 - self.cutoff) * N)
         elif self.cutoff >= 1:
             i_cut = int(N - self.cutoff)
-        else:
+        else: # cutoff < 0 là không hợp lệ
             raise ValueError('self.cutoff must be positive, not {0}'
                              ''.format(self.cutoff))
 
-        # create the mask; we zero-out values where the mask is True
-        N = len(self.full_tree_.data)
-        if i_cut < 0:
-            mask = np.ones(N, dtype=bool)
-        elif i_cut >= N:
-            mask = np.zeros(N, dtype=bool)
+        N = len(self.full_tree_.data) # số cạnh trong MST
+        if i_cut < 0: # Không cắt cạnh nào
+            mask = np.ones(N, dtype=bool) # mask của tất cả các cạnh là True
+        elif i_cut >= N: # số cạnh cần cắt lơn hơn hoặc bằng số cạnh MST
+            mask = np.zeros(N, dtype=bool) # mask toàn bộ = false
         else:
             mask = np.ones(N, dtype=bool)
-            part = np.argpartition(self.full_tree_.data, i_cut)
-            mask[part[:i_cut]] = False
+            part = np.argpartition(self.full_tree_.data, i_cut) # Lấy i_cut các cạnh nhỏ nhất của cây
+            mask[part[:i_cut]] = False # cạnh bị cắt = false
 
-        # additionally cut values above the ``cutoff_scale``
+        # Cắt cạnh có trọng số lớn hơn cutoff_scale
         if self.cutoff_scale is not None:
             mask |= (self.full_tree_.data > self.cutoff_scale)
 
         # Trim the tree
         cluster_graph = self.full_tree_.copy()
 
-        # Eliminate zeros from cluster_graph for efficiency.
-        # We want to do this:
-        #    cluster_graph.data[mask] = 0
-        #    cluster_graph.eliminate_zeros()
-        # but there could be explicit zeros in our data!
-        # So we call eliminate_zeros() with a stand-in data array,
-        # then replace the data when we're finished.
-        original_data = cluster_graph.data
-        cluster_graph.data = np.arange(1, len(cluster_graph.data) + 1)
-        cluster_graph.data[mask] = 0
-        cluster_graph.eliminate_zeros()
-        cluster_graph.data = original_data[cluster_graph.data.astype(int) - 1]
+        original_data = cluster_graph.data # Lưu trọng số gốc lại
+        cluster_graph.data = np.arange(1, len(cluster_graph.data) + 1) # Đổi giá trị trọng số để tính không bị nhầm với giá trị gốc
+        cluster_graph.data[mask] = 0 # Các cạnh cần cắt trọng số = 0
+        cluster_graph.eliminate_zeros() # Bỏ các cạnh trọng số = 0 đi
+        cluster_graph.data = original_data[cluster_graph.data.astype(int) - 1] # Khôi phục trọng số gốc theo index
 
-        # find connected components
+        # n_components lưu số lượng thành phần liên thông, labels lưu nhãn của từng đỉnh thuộc tplt nào
         n_components, labels = connected_components(cluster_graph,
                                                     directed=False)
 
-        # remove clusters with fewer than min_cluster_size
+        # bỏ các nhóm có kích thước nhỏ hơn min_cluster_size
         counts = np.bincount(labels)
         to_remove = np.where(counts < self.min_cluster_size)[0]
 
@@ -199,38 +140,3 @@ class MSTClustering(BaseEstimator, ClusterMixin):
         self.labels_ = labels
         self.cluster_graph_ = cluster_graph
         return self
-
-    def get_graph_segments(self, full_graph=False):
-        """Convenience routine to get graph segments
-
-        This is useful for visualization of the graph underlying the algorithm.
-
-        Parameters
-        ----------
-        full_graph : bool (default: False)
-            If True, return the full graph of connections. Otherwise return
-            the truncated graph representing clusters.
-
-        Returns
-        -------
-        segments : tuple of ndarrays
-            the coordinates representing the graph. The tuple is of length
-            n_features, and each array is of size (n_features, n_edges).
-            For n_features=2, the graph can be visualized in matplotlib with,
-            e.g. ``plt.plot(segments[0], segments[1], '-k')``
-        """
-        if not hasattr(self, 'X_fit_'):
-            raise ValueError("Must call fit() before get_graph_segments()")
-        if self.metric == 'precomputed':
-            raise ValueError("Cannot use ``get_graph_segments`` "
-                             "with precomputed metric.")
-
-        n_samples, n_features = self.X_fit_.shape
-
-        if full_graph:
-            G = sparse.coo_matrix(self.full_tree_)
-        else:
-            G = sparse.coo_matrix(self.cluster_graph_)
-
-        return tuple(np.vstack(arrs) for arrs in zip(self.X_fit_[G.row].T,
-                                                     self.X_fit_[G.col].T))
